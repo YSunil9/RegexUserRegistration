29770c899554da9ffc88d41b9a9e7f4896392155
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-24 18:16:57 +0530

    [SUNIL]:Validate First Name

---
+++ RegexProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RegexProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome To Regex User Registration Problem");
+            RegexClass obj = new RegexClass();           
+            Console.WriteLine("1.First Name");
+            int select = Convert.ToInt32(Console.ReadLine());
+            switch (select)
+            {
+                case 1:
+                    Console.WriteLine("Enter First Name");
+                    string firstName = Console.ReadLine();
+                    obj.FirstName(firstName);
+                    break;
+            }
+        }
+    }
+}
